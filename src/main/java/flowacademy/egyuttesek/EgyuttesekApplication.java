de759019337dd79ab4ba10bdd3afae6c292c4bbb
Peter Szabo <szabo.peter1991@gmail.com>
2021-05-26 13:42:37 +0200

    first commit

---
+++ src/main/java/flowacademy/egyuttesek/EgyuttesekApplication.java
@@ -0,0 +1,13 @@
+package flowacademy.egyuttesek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgyuttesekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgyuttesekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flowacademy/egyuttesek/EgyuttesekApplicationTests.java
@@ -0,0 +1,13 @@
+package flowacademy.egyuttesek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgyuttesekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
