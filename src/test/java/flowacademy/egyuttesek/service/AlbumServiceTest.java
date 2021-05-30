d1804cb2e25aaae405fb288963b369ced0d4a0d1
Peter Szabo <szabo.peter1991@gmail.com>
2021-05-30 20:01:58 +0200

    create a test

--- src/test/java/flowacademy/egyuttesek/service/AlbumServiceTest.java
+++ src/test/java/flowacademy/egyuttesek/service/AlbumServiceTest.java
@@ -3,6 +3,7 @@ package flowacademy.egyuttesek.service;
 import flowacademy.egyuttesek.model.Album;
 import flowacademy.egyuttesek.model.Band;
 import flowacademy.egyuttesek.repository.AlbumRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -19,29 +20,34 @@ import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class AlbumServiceTest {
-//    @InjectMocks
-//    AlbumService albumService;
-//
-//    @Mock
-//    AlbumRepository albumRepository;
-//
+    @InjectMocks
+    AlbumService albumService;
+
+    @Mock
+    AlbumRepository albumRepository;
+
+    @Test
+    public void findAllAlbumTest() {
+        List<Album> albumList = List.of(
+                Album.builder().id("ID01").name("Album Cím1").build(),
+                Album.builder().id("ID01").name("Album Cím2").build(),
+                Album.builder().id("ID01").name("Album Cím3").build()
+        );
+        when(albumRepository.findAll()).thenReturn(albumList);
+
+        assertEquals(3, albumService.findAll().size());
+    }
+
 //    @Test
-//    public void findByBandNameTest() {
-//        Band band1 = new Band(UUID.randomUUID(), "Metallica", "tipus", List.of());
-//        Band band2 = new Band(UUID.randomUUID(), "Mas", "tipus", List.of());
-//        Band band3 = new Band(UUID.randomUUID(), "Mas", "tipus", List.of());
+//    public void addAlbumTest(){
 //
-//        List<Album> albumList = List.of(
-//                new Album(UUID.randomUUID(), "AlbumCím", Date.valueOf(LocalDate.now()), band1, List.of()),
-//                new Album(UUID.randomUUID(), "AlbumCím2", Date.valueOf(LocalDate.now()), band2, List.of()),
-//                new Album(UUID.randomUUID(), "AlbumCím3", Date.valueOf(LocalDate.now()), band3, List.of())
-//
-//        );
-//        when(albumRepository.findAll()).thenReturn(albumList);
-//
-//        List<Album> albums = albumService.findByName("AlbumCím");
-//
-//        assertEquals(1, albums.size());
+//        albumRepository.save(Album.builder().id("ID01").name("Album Cím1").build());
+//        assertEquals(1,albumService.findAll().size());
 //    }
+
+//    Band band1 = Band.builder().id("ID1").name("Metallica").build();
+//    Band band2 = Band.builder().id("ID2").name("AC/DC").build();
+//    Band band3 = Band.builder().id("ID3").name("Kispál és a Borz").build();
+
 
 }
